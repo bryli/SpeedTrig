25e5bb09f24c909dd4542720078696b13022137b
Bryan Li <bryanli0@icloud.com>
2018-10-18 22:57:32 -0400

    Added LaTeX template.

---
+++ SpeedTrig.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, url_for, redirect, render_template, session
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
---
+++ TrigGen.py
@@ -0,0 +1,11 @@
+
+
+SIN = "\sin"
+COS = "\cos"
+TAN = "\\tan"
+CSC = "\csc"
+
+
+trigBase = ["sin", "cos", "tan"]
+trigReci = ["csc", "sec", "cot"]
+trig
