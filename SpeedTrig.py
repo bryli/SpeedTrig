4e19d05e5195c8e2e0c1afa236939da5ec1fa56a
Gautom Das <das@Gautoms-MacBook-Pro.local>
2018-10-19 20:29:08 -0400

    Radio Button Broken

--- SpeedTrig.py
+++ SpeedTrig.py
@@ -9,7 +9,48 @@ def home():
 
 @app.route('/generated', methods=['GET', 'POST'])
 def generated():
-    print(request.form.get("chance"))
+    ### norm is a Boolean for whether the Normal Trig Functions option was selected.
+    # True if selected
+    # False if not selected
+    norm = False
+    if(request.form.get("norm") == "on"):
+        norm = True
+    print(norm)
+
+    ### reci is a Boolean for whether the Reciprocal Trig Functions option was selected.
+    # True if selected
+    # False if not selected
+    reci = False
+    if(request.form.get("reci") == "on"):
+        reci = True
+    print(reci)
+
+    ### invnorm is a Boolean for whether the Inverse Normal Trig Functions option was selected.
+    # True if selected
+    # False if not selected
+    invnorm = False
+    if(request.form.get("invnorm") == "on"):
+        invnorm = True
+    print(invnorm)
+
+    ### invreci is a Boolean for whether the Inverse Reciprocal Trig Functions option was selected.
+    # True if selected
+    # False if not selected
+    invreci = False
+    if(request.form.get("invreci") == "on"):
+        invreci = True
+    print(invreci)
+
+    ### override is a Boolean for whether the user wants exact number or perecnt.
+    # True if selected
+    # False if not selected
+    override = False
+    if(request.form.get("override") == "on"):
+        override = True
+    print(override)
+
+    chance = int(request.form.get("chance"))
+    print(chance)
     return render_template('generated.html')
 
 
